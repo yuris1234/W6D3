176b42a53f2f79402bf5a5336516a08c9befbb07
“Spencer <Spencer@bookservo.com>
2023-09-27 13:52:08 -0400

    routes written out

--- practice-for-ch-rails-first-routes-long-practice-main/practice/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice/config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   # root "articles#index"
   # resources :users
 
-  
+  get "/users", to: "users#index"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  get "/users/new", to: "users#new", as: "new_user"
+  patch "/users/:id", to: "users#update"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
 end
