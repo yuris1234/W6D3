491c569b4007391f7b3363e7b01ae337d4d1a70f
yuris <yls4@cornell.edu>
2023-09-27 15:05:55 -0400

    almost done with phase 4

--- practice-for-ch-rails-first-routes-long-practice-main/practice/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice/app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
     def index 
-        render plain: "I'm in the index action!"
-        # users = User.all 
-        # render json: users
+        # render plain: "I'm in the index action!"
+        users = User.all 
+        render json: users
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+          render json: user
+        else
+          render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
